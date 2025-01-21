bf87ea4dae365c7ece80d58fdf1292e336a1f5fc
Svetlana Markova <80004406+marss-hub@users.noreply.github.com>
2025-01-22 00:11:33 +0500

    add configData example file

---
+++ configData.mjs
@@ -0,0 +1,10 @@
+export const CONFIGDATA = {
+  api_key_bot: "{YOUR_TELEGRAM_BOT_TOKEN}", 
+  sav_api_url: "https://api.savagedrx.com/stats/live",
+  log_file_path: "/home/user/sav-activity-bot/logs.txt", // example path
+  user_list_path: "/home/user/sav-activity-bot/users.txt", // example path
+  tracked_sever_ip: "XX.XX.XX.XX:XXXXX", // server ip
+  tracked_quantity_players: 20,
+  server_request_ms: 50000, // 50 sec
+  message_interval_ms: 1800000, //30 min
+};
